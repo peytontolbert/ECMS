d2bd6aa1cb9791f6b65428c745b5f88f22d85e2c
peytontolbert <peytonct@gmail.com>
2022-06-26 21:55:03 -0400

    commit on master

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 443;
 
 const server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.send('Hello World');
+  res.end('Hello World');
 });
 
 var indexRouter = require('./routes/index');

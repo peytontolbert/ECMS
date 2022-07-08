be90b0a79d144b28eda901567aa858a915e41a5c
peytontolbert <peytonct@gmail.com>
2022-07-08 12:11:48 -0400

    commit on master

--- app.js
+++ app.js
@@ -89,5 +89,186 @@ app.get('/system1historydata', function(req, res) {
     })
 });
 
+
+//LOGIN (AUTHENTICATE USER)
+app.post("/login", async (req, res)=> {
+    const username = req.body.username
+    const password = req.body.password
+    const sqlSearch = "Select * from employees where username = ?"
+    const search_query = mysql.format(sqlSearch,[username])
+
+     await con.query (search_query, async (err, result) => {
+      
+      if (err) throw (err)
+      if (result.length == 0) {
+       console.log("--------> User does not exist")
+       res.sendStatus(404)
+      } 
+      else {
+         const hashedPassword = result[0].password
+         //get the hashedPassword from result
+        if (await bcrypt.compare(password, hashedPassword)) {
+        console.log("---------> Login Successful")
+	    res.redirect('/systemselection');
+        } 
+        else {
+        console.log("---------> Password Incorrect")
+        res.send("Password incorrect!")
+        } //end of bcrypt.compare()
+      }//end of User exists i.e. results.length==0
+     }) //end of connection.query()
+    }) //end of app.post()
+
+
+    app.post("/savesystem1", async (req,res) => {
+        console.log(req.body);
+        const username = req.body.username;
+        const status = req.body.status;
+        const valve = req.body.valve;
+        const sqlSearch = "SELECT * FROM system1 WHERE valve = ?"
+        const search_query = mysql.format(sqlSearch,[valve])
+        const sqlInsert = "INSERT INTO system1 VALUES (?,?,?)"
+        const insert_query = mysql.format(sqlInsert,[valve, status, username])
+        const sqlUpdate = "UPDATE `system1` SET `status` = ?, `username` = ? WHERE `valve`= ?"
+        const update_query = mysql.format(sqlUpdate,[status, username, valve])
+        await con.query (search_query, async (err, result) => {
+            if (err) throw (err)
+            console.log("search results")
+            console.log(result.length)
+            if (result.length == 0) {
+                console.log("valve not found: " + valve)
+                console.log("creating new line")
+                await con.query (insert_query, (err, result) => {
+                    if (err) throw (err)
+                    console.log ("Created new valve " + valve)
+                    console.log(result.insertId)
+                    res.redirect("/system1")
+                })
+            } else {
+                console.log("initiating update")
+                await con.query (update_query, (err, result, row, fields) => {
+                    if (err) throw (err)
+                    console.log ("Updating valve " + valve)
+                    console.log(result)
+                    console.log(row)
+                    res.redirect("/system1")
+                })
+            }
+        })   // end of con.query
+    }); // end of app.post
+
+    app.post("/savevalve", async (req,res) => {
+        console.log(req.body);
+        const valve = req.body.valve;
+        const status = req.body.status;
+        const sqlSearch = "SELECT * FROM system1 WHERE valve = ?"
+        const search_query = mysql.format(sqlSearch,[valve])
+        const sqlInsert = "INSERT INTO system1 VALUES (?,?)"
+        const sqlInsertHistory =  "INSERT INTO system1history (valve, status) VALUES (?,?)"
+        const insert_query = mysql.format(sqlInsert,[valve, status])
+        const insertHistory_query = mysql.format(sqlInsertHistory,[valve, status])
+        const sqlUpdate = "UPDATE `system1` SET `status` = ? WHERE `valve`= ?"
+        const update_query = mysql.format(sqlUpdate,[status, valve])
+        await con.query (search_query, async (err, result) => {
+            if (err) throw (err)
+            console.log("search results")
+            console.log(result.length)
+            if (result.length == 0) {
+                console.log("valve not found: " + valve)
+                console.log("creating new line")
+                await con.query (insert_query, (err, result) => {
+                    if (err) throw (err)
+                    console.log ("Created new valve " + valve)
+                    console.log(result.insertId)
+                })
+                await con.query (insertHistory_query, (err, result) => {
+                    if (err) throw (err)
+                    console.log("Adding history")
+                })
+                res.redirect("/system1")
+            } else {
+                console.log("initiating update")
+                await con.query (update_query, (err, result, row, fields) => {
+                    if (err) throw (err)
+                    console.log ("Updating valve " + valve)
+                    console.log(result)
+                    console.log(row)
+                })
+                await con.query (insertHistory_query, (err, result) => {
+                    if (err) throw (err)
+                    console.log("Adding history")
+                })
+                res.redirect("/system1")
+            }
+        })   // end of con.query
+    }); // end of app.post
+        
+    app.post("/removewaf", async (req,res) => {
+        console.log(req.body);
+        const valve = req.body.valve;
+        const waf = req.body.waf;
+        const sqlSearch = "SELECT * FROM system1wafs WHERE valve = ?"
+        const search_query = mysql.format(sqlSearch,[valve])
+        const sqlDelete = "DELETE FROM system1wafs WHERE valve = ? AND waf = ?"
+        const delete_query = mysql.format(sqlDelete,[valve, waf])
+        await con.query (sqlSearch, async (err, result) => {
+            if (err) throw (err)
+            console.log("search results")
+            console.log(result.length)
+            if (result.length == 0) {
+                console.log("valve and waf not found " + valve + waf)
+                res.send("valve+waf does not exist");
+            } else {
+                console.log("initiating update")
+                await con.query (delete_query, (err, result, row, fields) => {
+                    if (err) throw (err)
+                    console.log ("deleting waf " + waf + " on valve " + valve)
+                    console.log(result)
+                }) 
+                res.redirect("/system1")
+            }
+        })   // end of con.query
+    }); // end of app.post
+        
+app.post("/register", async (req,res) => {
+    const username = req.body.username;
+    const hashedPassword = await bcrypt.hash(req.body.password,10);
+     const sqlSearch = "SELECT * FROM employees WHERE username = ?"
+     const search_query = mysql.format(sqlSearch,[username])
+     const sqlInsert = "INSERT INTO employees VALUES (0,?,?)"
+     const insert_query = mysql.format(sqlInsert,[username, hashedPassword])
+     // ? will be replaced by values
+     // ?? will be replaced by string
+     await con.query (search_query, async (err, result) => {
+      if (err) throw (err)
+      console.log("------> Search Results")
+      console.log(result.length)
+      if (result.length != 0) {
+       console.log("------> User already exists: " + username)
+       res.sendStatus(409) 
+      } 
+      else {
+       await con.query (insert_query, (err, result)=> {
+       if (err) throw (err)
+       console.log ("--------> Created new User: " + username)
+       console.log(result.insertId)
+       res.sendStatus(201)
+      })
+     }
+    }) //end of con.query()
+    }) //end of app.post()
+
+
+
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
 server.listen(process.env.PORT || 443); 
 
